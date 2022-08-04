108a6bea9d9e528ebf30f669579e4a72aaf33002
richard <richard.marrugo@itxpertix.com>
2022-08-04 10:18:36 -0500

    Se quita paginacion del servicio de actividades

--- src/main/java/com/rmarrugo/test/domain/activity/port/ActivityFinder.java
+++ src/main/java/com/rmarrugo/test/domain/activity/port/ActivityFinder.java
@@ -1,15 +1,13 @@
 package com.rmarrugo.test.domain.activity.port;
 
 import com.rmarrugo.test.domain.activity.Activity;
-import com.rmarrugo.test.domain.common.PageAsk;
 
-import java.util.function.Supplier;
-import java.util.stream.Stream;
+import java.util.List;
 
 public interface ActivityFinder {
 
     Activity findById(Long id);
 
-    Supplier<Stream<Activity>> find(PageAsk pageAsk);
+    List<Activity> findAll();
 
 }
